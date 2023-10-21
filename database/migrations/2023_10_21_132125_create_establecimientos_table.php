18346732212fb6658ed696ba986dc1a6f05c05bf
Cruz Wilfrido Ek Martin <gamecruz@gmail.com>
2023-10-21 13:40:58 -0500

    se crean migraciones para usuarios

---
+++ database/migrations/2023_10_21_132125_create_establecimientos_table.php
@@ -0,0 +1,44 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('establecimientos', function (Blueprint $table) {
+            $table->id();
+            $table->string('nombre');
+            $table->foreignId('categoria_id')->constrained();
+            $table->string('imagen_principal');
+            $table->string('direccion');
+            $table->string('colonia');
+            $table->string('lat');
+            $table->string('lng');
+            $table->string('telefono');
+            $table->text('descripcion');
+            $table->time('apertura');
+            $table->time('cierre');
+            $table->uuid('uuid');
+            $table->foreignId('user_id')->constrained();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('establecimientos');
+    }
+};
---
+++ database/seeders/UsuarioSeed.php
@@ -0,0 +1,39 @@
+<?php
+
+namespace Database\Seeders;
+
+use Carbon\Carbon;
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Support\Facades\Hash;
+
+class UsuarioSeed extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        DB::table('users')->insert([
+            'name' => 'ADMIN',
+            'email' => 'admin@gmail.com',
+            'email_verified_at' => Carbon::now(),
+            'password' => Hash::make('12345678'),
+            'created_at' => Carbon::now(),
+            'updated_at' => Carbon::now()
+        ]);
+
+        
+        DB::table('users')->insert([
+            'name' => 'CLIENTE1',
+            'email' => 'cliente1@gmail.com',
+            'email_verified_at' => Carbon::now(),
+            'password' => Hash::make('12345678'),
+            'created_at' => Carbon::now(),
+            'updated_at' => Carbon::now()
+        ]);
+    }
+}
